39334e1b2c3d46149b3201e7b0055f03a0b665a2
Sam Fuchs <samcfuchs@gmail.com>
2017-06-14 15:14:12 -0400

    Fix solid.ino

--- solid/solid.ino
+++ solid/solid.ino
@@ -1,17 +1,19 @@
 #include <Adafruit_NeoPixel.h>
 
 #define PIN 6
+#define N 300
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(60, PIN, NEO_RGBW + NEO_KHZ800);
+
+uint32_t color = strip.Color(30, 20, 200);
 
 void setup() {
   strip.begin();
+
+  for(int i = 0; i < N; i++) {
+    strip.setPixelColor(i, color);
+  }
   strip.show();
 }
 
 void loop() {
-  for (int i = 0; i < 60; i++) {
-    strip.setPixelColor(i, 0, 0, 255);
-  }
-  strip.show();
-  delay(100);
 }
